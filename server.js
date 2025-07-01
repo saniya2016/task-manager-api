3e70173a406ed9cdef5fbdad7ff831c868b0171e
nut.cracker <141542236+saniya2016@users.noreply.github.com>
2025-07-01 21:31:17 +0530

    server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const app = require('./src/app');
-const { connectDB } = require('./src/config/database'); // ✅ Use destructuring here
+const { connectDB } = require('./src/config/database'); 
 const { PORT } = require('./src/config/serverConfig');
 
 const startServer = async () => {
   try {
     await connectDB();
     app.listen(PORT, () => {
-      console.log(`🚀 Server running at http://localhost:${PORT}`);
+      console.log(` Server running at http://localhost:${PORT}`);
     });
   } catch (error) {
-    console.error('❌ Failed to start server:', error.message);
+    console.error('Failed to start server:', error.message);
   }
 };
 
